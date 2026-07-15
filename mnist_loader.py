6daa82a2468b3dfa957fd66dc3e5601c38fbb237
Mohamad Ali Chamseddine <78427932+mohamadalichamseddine@users.noreply.github.com>
2026-07-14 21:01:02 -0400

    Add data loader (#3)

---
+++ mnist_loader.py
@@ -0,0 +1,74 @@
+import pickle
+import gzip
+
+import numpy as np
+
+def load_data():
+    # returned tuple
+    # │
+    # ├── training_data tuple
+    # │   ├── images ndarray
+    # │   └── labels ndarray
+    # │
+    # ├── validation_data tuple
+    # │   ├── images ndarray
+    # │   └── labels ndarray
+    # │
+    # └── test_data tuple
+    #     ├── images ndarray
+    #     └── labels ndarray
+
+    with gzip.open("data/mnist.pkl.gz", "rb") as pickle_file:
+        loaded_object = pickle.load(pickle_file)
+        training_data, validation_data, test_data = loaded_object
+
+    return (training_data, validation_data, test_data)
+
+def load_data_wrapper():
+    # returned tuple
+    # │
+    # ├── training_data
+    # │   └── 50,000 tuples: (image column vector, one-hot label)
+    # │
+    # ├── validation_data
+    # │   └── 10,000 tuples: (image column vector, integer label)
+    # │
+    # └── test_data
+    #     └── 10,000 tuples: (image column vector, integer label)
+
+    tr_d, va_d, te_d = load_data()
+
+    training_inputs = []
+    for image in tr_d[0]:
+        reshaped_image = np.reshape(image, (784, 1))
+        training_inputs.append(reshaped_image)
+
+    training_results = []
+    for label in tr_d[1]:
+        one_hot_label = vectorized_result(label)
+        training_results.append(one_hot_label)
+
+    training_data = zip(training_inputs, training_results)
+
+    validation_inputs = []
+    for image in va_d[0]:
+        reshaped_image = np.reshape(image, (784, 1))
+        validation_inputs.append(reshaped_image)
+
+    validation_data = zip(validation_inputs, va_d[1])
+
+    test_inputs = []
+    for image in te_d[0]:
+        reshaped_image = np.reshape(image, (784, 1))
+        test_inputs.append(reshaped_image)
+
+    test_data = zip(test_inputs, te_d[1])
+
+    return training_data, validation_data, test_data
+
+
+def vectorized_result(j):
+    # Convert a digit from 0 to 9 into a one-hot column vector
+    result = np.zeros((10, 1))
+    result[j] = 1.0
+    return result
